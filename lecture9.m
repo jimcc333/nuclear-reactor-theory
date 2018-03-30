3a1fa021244564ca857301ec37822d8e865d1b01
jimcc333 <jimcc333@gmail.com>
2018-03-29 20:54:14 -0500

    adds title and axis labels to lecture9.m

--- lecture9.m
+++ lecture9.m
@@ -5,7 +5,7 @@
 % methods and compares the outputs
 
 %% Matrix Solution
-mesh = 31;                      % Number of mech points
+mesh = 21;                      % Number of mech points
 dx = 3/(mesh-1);
 x = linspace(0,3,mesh);         % populate x with points spanning [0,3]
 S = ones(1,mesh);               % uniform volume source
@@ -16,14 +16,17 @@ A(mesh,mesh) = 1;  S(mesh) = 0; % boundary condition fN=0
 for i = 2:mesh-1
     A(i,i-1) = -1/(dx)^2;       % coefficient of fi-1
     A(i,i+1) = -1/(dx)^2;       % coefficient of fi+1
-    A(i,i)   = 2/(dx)^2 + 2;    % coefficient of fi
+    A(i,i)   = 2/(dx)^2 + 1;    % coefficient of fi
 end
-phi=linsolve(A,S');             % MATLAB calculates f=A-1S.
-plot(x, phi, '+')               % Plot numerical solution with plus signs
+phi_numeric=A\S';                       % MATLAB calculates f=A-1S.
+plot(x, phi_numeric, '+')               % Plot numerical solution with plus signs
 hold on;
 
 %% Exact (Theoretical) Solution
-exact_phi = 1 - cosh(x-1.5)/cosh(1.5);  % Find the symmetric and exact solution
-plot(x,exact_phi)                       % Plot the exact solution
-legend('numerical solution', 'exact solution')
+phi_exact = 1 - cosh(x-1.5)/cosh(1.5);  % Find the symmetric and exact solution
+plot(x,phi_exact)                       % Plot the exact solution
+title('Neutron Flux In A Slab')
+legend('Numerical solution', 'Exact solution')
+xlabel('x (cm)')
+ylabel('Neutron Flux (normalized units)')
 
